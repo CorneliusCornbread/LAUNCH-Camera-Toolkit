ca791589b54bd6d06f53b89e009a9f4ce97c942a
Jack Rosenbecker <link2003.2013@gmail.com>
2021-02-17 11:06:13 -0600

    Camera smooth moving

--- LAUNCH Test Project/Assets/Scripts/CameraMove.cs
+++ LAUNCH Test Project/Assets/Scripts/CameraMove.cs
@@ -2,8 +2,55 @@
 
 namespace CameraToolkit
 {
-	public class CameraMove : MonoBehaviour
-	{
-		
-	}
+    public class CameraMove : MonoBehaviour, IVRGrabbable
+    {
+        public bool interpolatedMove = false;
+        public float lerpMult = 1;
+
+        private Vector3 _oldPosition;
+        private Quaternion _oldRotation;
+
+        private Vector3 _targetPosition;
+        private Quaternion _targetRotation;
+
+        private float _time = 0f;
+
+        private void Update()
+        {
+            if (interpolatedMove)
+            {
+                _time += Time.deltaTime;
+                transform.position = Vector3.Lerp(_oldPosition, _targetPosition, _time * lerpMult);
+
+                transform.rotation = Quaternion.Lerp(_oldRotation, _targetRotation, _time * lerpMult);
+            }
+        }
+
+        public void Grab()
+        {
+            
+        }
+
+        public void Release()
+        {
+
+        }
+
+        public void Move(Vector3 position, Quaternion rotation)
+        {
+            if (interpolatedMove)
+            {
+                _oldPosition = _targetPosition;
+                _oldRotation = _targetRotation;
+
+                _targetPosition = position;
+                _targetRotation = rotation;
+            }
+            else
+            {
+                transform.position = position;
+                transform.rotation = rotation;
+            }
+        }
+    }
 }
---
+++ LAUNCH Test Project/Assets/Scripts/IVRGrabbable.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace CameraToolkit
+{
+	public interface IVRGrabbable
+	{
+		/// <summary>
+		/// Called when the user firsts grabs the object
+		/// </summary>
+		void Grab();
+
+		/// <summary>
+		/// Called when the user releases the object
+		/// </summary>
+		void Release();
+
+		/// <summary>
+		/// Called every time the position changes
+		/// </summary>
+		/// <param name="position">New position</param>
+		/// <param name="rotation">New rotation</param>
+		void Move(Vector3 position, Quaternion rotation);
+	}
+}
