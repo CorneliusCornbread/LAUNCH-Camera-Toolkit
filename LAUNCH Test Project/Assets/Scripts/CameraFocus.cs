e96a44e1ae6df3adf8c35efe6d1f86f6ac7a955b
Andrew-Prudlow <22prudla1@elmbrookstudents.org>
2021-02-19 10:26:20 -0600

    Super basic camera focus

---
+++ LAUNCH Test Project/Assets/Scripts/CameraFocus.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace CameraToolkit
+{
+	public class CameraFocus : MonoBehaviour
+	{
+		public Transform target;
+
+        private void Update()
+        {
+            Camera.main.transform.LookAt(target.position);
+        }
+    }
+}
