655714412e36f56bbddeee340977bbfa69832833
Jack Rosenbecker <link2003.2013@gmail.com>
2021-02-22 11:30:23 -0600

    Interpolated rotation

--- LAUNCH Test Project/Assets/Scripts/Interpolator.cs
+++ LAUNCH Test Project/Assets/Scripts/Interpolator.cs
@@ -5,57 +5,84 @@ namespace CameraToolkit
 {
     public class Interpolator : MonoBehaviour
     {
-        public bool interpolatePosition = false;
-        public bool interpolateRotation = false;
-
-        [SerializeField]
-        [Tooltip("How fast the position interpolation will move in meters per second.")]
-        [Range(.1f, 10)]
-        private float positionInterpSpeed = 0.25f;
-
-        [Tooltip("The number of times per second the target position is updated. Default is 60.")]
+        [Tooltip("The number of times per second the target position is updated. Default is 60. A lower number could improve performance.")]
         [Range(1, 240)]
         public uint updatesPerSecond = 60;
+
+        #region Position Serialized Fields
+        [Space(20)]
+        public bool interpolatePosition = true;
+
+        [SerializeField]
+        [Tooltip("How fast the position interpolation will move in percent per second. This means the effective speed will change with the distance.")]
+        [Range(.1f, 10)]
+        private float positionInterpSpeed = 4;
+
+        [SerializeField]
+        [Tooltip("If the distance is closer than this then the interpolator will snap to it")]
+        [Range(.001f, .025f)]
+        private float errorPosSnapDist = .008f;
 
         /// <summary>
         /// The target position to move to
         /// </summary>
         public Vector3 targetPosition;
+        #endregion
+
+        #region Rotation Serialized Fields
+        [Space(20)]
+        public bool interpolateRotation = true;
+
+        [SerializeField]
+        [Tooltip("How fast the rotation interpolation will move in percent per second. This means the effective speed will change with the distance.")]
+        [Range(.1f, 10)]
+        private float rotationInterpSpeed = 3;
+
+        [SerializeField]
+        [Tooltip("If the distance is closer than this then the interpolator will snap to it")]
+        [Range(.001f, .025f)]
+        private float errorRotSnapDist = .025f;
 
         /// <summary>
         /// The target rotation to move to
         /// </summary>
         public Quaternion targetRotation;
+        #endregion
 
+        #region Private Non-Serialized Fields
         private Vector3 _currentPosTarget;
         private Quaternion _currentRotTarget;
 
         private Vector3 _startPosition;
         private Quaternion _startRotation;
 
-        //Used to scale the speed as the interpolation distance increases or decreases
-        private float _targetDist;
-        private float _rotationInterpScale;
+        private float _targetPosDist;
+        private float _targetRotDist;
 
         //Percent interpolation
         private float _positionInterp;
         private float _rotationInterp;
+        #endregion
 
-        private void Start()
+        //We use OnEnable as disabling the parent game object will stop this coroutine
+        private void OnEnable()
         {
             StartCoroutine(Interpolate());
         }
 
         private void Update()
         {
-            if (interpolatePosition)
+            if (interpolatePosition && _targetPosDist > errorPosSnapDist)
             {
-                _positionInterp += (Time.deltaTime * positionInterpSpeed) / _targetDist;
-                //_positionInterp *= GetEasing(_positionInterp);
-
-                //TODO: setup easing
+                _positionInterp += Time.deltaTime * positionInterpSpeed;
 
                 transform.position = Vector3.Lerp(_startPosition, _currentPosTarget, _positionInterp);
+            }
+            if (interpolateRotation && _targetRotDist > errorRotSnapDist)
+            {
+                _rotationInterp += Time.deltaTime * positionInterpSpeed;
+
+                transform.rotation = Quaternion.Slerp(_startRotation, _currentRotTarget, _rotationInterp);
             }
         }
 
@@ -63,38 +90,50 @@ namespace CameraToolkit
         {
             while (true)
             {
-                if (!enabled || !gameObject.activeInHierarchy) { continue; } //Coroutines can run regardless of a script or game object being enabled
+                //Coroutines can run regardless of a script or game object being enabled
+                if (!enabled) { continue; } 
 
-                float updateDelay = 1 / updatesPerSecond; //We do this calculation every coroutine loop as it could change at runtime
+                //We do this calculation every coroutine loop as it could change at runtime
+                float updateDelay = 1 / Mathf.Clamp(updatesPerSecond, .00001f, Mathf.Infinity); 
                 yield return new WaitForSeconds(updateDelay);
 
                 if (interpolatePosition)
                 {
-                    _startPosition = transform.position;
-                    _positionInterp = 0;
+                    _targetPosDist = Vector3.Distance(_startPosition, targetPosition);
 
-                    _currentPosTarget = targetPosition;
-                    _targetDist = Vector3.Distance(_startPosition, targetPosition);
+                    if (_targetPosDist > errorPosSnapDist) 
+                    {
+                        _startPosition = transform.position;
+                        _positionInterp = 0;
+
+                        _currentPosTarget = targetPosition;
+                    }
+                    else
+                    {
+                        _startPosition = targetPosition;
+                        _currentPosTarget = targetPosition;
+                        transform.position = targetPosition;
+                    }
                 }
-
                 if (interpolateRotation)
                 {
-                    _startRotation = transform.rotation;
-                    _rotationInterp = 0;
+                    _targetRotDist = Quaternion.Angle(_startRotation, targetRotation) / 2;
 
-                    _currentRotTarget = targetRotation;
+                    if (_targetRotDist > errorRotSnapDist)
+                    {
+                        _startRotation = transform.rotation;
+                        _rotationInterp = 0;
+
+                        _currentRotTarget = targetRotation;
+                    }
+                    else
+                    {
+                        _startRotation = targetRotation;
+                        _currentRotTarget = targetRotation;
+                        transform.rotation = targetRotation;
+                    }
                 }
             }
-        }
-
-        /// <summary>
-        /// A function used to ease the interpolation
-        /// </summary>
-        /// <param name="interp">Current interpolation value</param>
-        /// <returns></returns>
-        private float GetEasing(float interp)
-        {
-            return Mathf.Sin((2.5f * Mathf.Clamp(interp, 0, 1)) + .32f) * 2;
         }
     }
 }
