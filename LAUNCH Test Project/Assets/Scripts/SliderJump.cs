a2cfc7d99c81a56cabb5dd50cb4a4b977a394f3c
Nick Hanoski <nickhanoski34@gmail.com>
2021-02-23 15:35:30 -0600

    unfinished UI testing
    unfinished UI testing

---
+++ LAUNCH Test Project/Assets/Scripts/SliderJump.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+
+public class SliderJump : MonoBehaviour
+{	
+	[SerializeField]
+	private Slider mainSlider;
+
+	[SerializeField]
+    private float speed = 9.0f;
+
+	[SerializeField]
+    private float startPosition;
+
+	
+
+    public void OnSliderMove()
+    {
+		
+        startPosition = transform.position.y+ (mainSlider.value * 10);
+
+		transform.position = new Vector3(transform.position.x, 
+		startPosition + Mathf.Sin(Time.time * speed), 
+		transform.position.z);
+
+		Debug.Log(mainSlider.value); // debug
+    } 
+}
