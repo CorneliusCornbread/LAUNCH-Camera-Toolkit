b6571b8ac23f535ee391ab65d0ff96a2e9730311
Nick Hanoski <nickhanoski34@gmail.com>
2021-02-23 15:35:30 -0600

    unfinished UI testing
    unfinished UI testing

---
+++ LAUNCH Test Project/Assets/Scripts/ButtonDetect.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDetect : MonoBehaviour {
+   int n;
+   public void OnButtonPress() {
+      n++;
+      Debug.Log("Button clicked " + n + " times.");
+   }
+}
