e8608dd379ae9d3b27e582238c688eb102e79b7c
Jack Rosenbecker <link2003.2013@gmail.com>
2021-02-23 11:26:55 -0600

    Starting on multi camera

---
+++ LAUNCH Test Project/Assets/Scripts/MultiCameraManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CameraToolkit
+{
+	public class MultiCameraManager : MonoBehaviour
+	{
+		[SerializeField]
+		private List<Camera> cameras;
+
+		public List<Camera> Cameras => cameras;
+
+        private void Start()
+        {
+            
+        }
+    }
+}
