a67c30c7dd543841198774d462f0f9930b44bd2b
Nick Hanoski <nickhanoski34@gmail.com>
2021-02-24 11:30:53 -0600

    Added change x and y buttons

---
+++ LAUNCH Test Project/Assets/Scripts/ChangeY.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace CameraToolkit
+{
+	public class ChangeY : MonoBehaviour
+	{
+
+		public void OnButtonYUpPressed()
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y + 1,
+				transform.position.z);
+		}
+
+		public void OnButtonYDownPressed()
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y - 1,
+				transform.position.z);
+		}
+	}
+}
---
+++ LAUNCH Test Project/Assets/Scripts/ChangeZ.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace CameraToolkit
+{
+	public class ChangeZ : MonoBehaviour
+	{
+
+		public void OnButtonZForwardPressed()
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y,
+				transform.position.z + 1);
+		}
+
+		public void OnButtonZBackPressed()
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
+		}
+	}
+}
