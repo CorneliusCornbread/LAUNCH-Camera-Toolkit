830948362376561d62c60005bacebaad3eeac284
Jack Rosenbecker <link2003.2013@gmail.com>
2021-02-25 11:26:21 -0600

    Changes

---
+++ LAUNCH Test Project/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,231 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CameraToolkit
+{
+    /// <summary>
+    /// A singleton which manages switching between many cameras.
+    /// </summary>
+    [DisallowMultipleComponent]
+    public class CameraManager : MonoBehaviour
+    {
+        #region Singleton
+        /// <summary>
+        /// The currently active singleton instance. Will automatically create a new singleton if one does not exist.
+        /// </summary>
+        public static CameraManager Instance 
+        {
+            get 
+            {
+                //Create an instance of our singleton if it doesn't not already exist
+                if (_instance == null)
+                {
+                    GameObject gObj = new GameObject("Camera Manager");
+                    gObj.AddComponent<CameraManager>();
+                    Instantiate(gObj);
+
+                    return gObj.GetComponent<CameraManager>();
+                }
+                else
+                {
+                    return _instance;
+                }
+            }
+        }
+
+        private static CameraManager _instance;
+        #endregion
+
+        [SerializeField]
+        private List<ManagedCamera> cameras;
+
+        public int ActiveCameraIndex { get; private set; } = -1;
+
+        public int CameraCount => cameras.Count;
+
+        /// <summary>
+        /// Indexes the camera collection within the CameraManager.
+        /// </summary>
+        /// <param name="i">Index</param>
+        /// <returns>managed Cam at that index</returns>
+        public ManagedCamera this[int i]
+        {
+            get { return cameras[i]; }
+        }
+
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Debug.LogError("Cannot have more than one instance of CameraManager active at once." +
+                    $"Disabling 2nd instance \"{gameObject.name}\"");
+                enabled = false;
+                return;
+            }
+        }
+
+        private void Start()
+        {
+            if (_instance != null && _instance != this) { return; } 
+
+            _instance = this;
+            DontDestroyOnLoad(this);
+
+            //Update the serialized cameras to have the correct index
+            for (int i = 0; i < cameras.Count; i++)
+            {
+                cameras[i].cameraIndex = i;
+            }
+        }
+
+        /// <summary>
+        /// Disables the currently active camera.
+        /// </summary>
+        public void DisableActiveCamera()
+        {
+            if (ActiveCameraIndex < 0) { return; }
+
+            cameras[ActiveCameraIndex].SetCameraActive(false);
+            ActiveCameraIndex = -1;
+        }
+
+        /// <summary>
+        /// Changes the active camera to the camera at the given collection index.
+        /// </summary>
+        /// <param name="cameraIndex">Camera to change to</param>
+        public void ChangeActiveCamera(int cameraIndex)
+        {
+            if (cameraIndex < 0 || cameraIndex > cameras.Count - 1)
+            {
+                throw new System.ArgumentOutOfRangeException(nameof(cameraIndex));
+            }
+
+            if (ActiveCameraIndex > -1)
+            {
+                cameras[ActiveCameraIndex].SetCameraActive(false);
+            }
+
+            cameras[cameraIndex].SetCameraActive(true);
+            ActiveCameraIndex = cameraIndex;
+        }
+
+        /// <summary>
+        /// Changes the active camera to the given camera. Will error out if this camera hasn't been added yet.
+        /// </summary>
+        /// <param name="cam"></param>
+        public void ChangeActiveCamera(ManagedCamera cam)
+        {
+            if (cam == null)
+            {
+                throw new System.ArgumentNullException(nameof(cam));
+            }
+
+            if (cam.cameraIndex < 0)
+            {
+                Debug.LogError("Camera passed in ChangeActiveCamera() has not been assigned an valid index." +
+                    "Has this item not been added to the CameraManager yet?");
+                return;
+            }
+
+            ChangeActiveCamera(cam.cameraIndex);
+        }
+
+        /// <summary>
+        /// Changes the active camera to the next camera in the managed camera collection.
+        /// </summary>
+        public void ChangeToNextCamera()
+        {
+            if (cameras.Count == 0)
+            {
+                Debug.LogWarning("Tried to change to the next camera in managed Cam when there are no cameras setup.");
+                return;
+            }
+
+            int newInd = ActiveCameraIndex + 1;
+
+            if (newInd > cameras.Count - 1)
+            {
+                ChangeActiveCamera(0);
+            }
+            else
+            {
+                ChangeActiveCamera(newInd);
+            }
+        }
+
+        /// <summary>
+        /// Changes the active camera to the previous camera in the managed camera collection.
+        /// </summary>
+        public void ChangeToPreviousCamera()
+        {
+            if (cameras.Count == 0)
+            {
+                Debug.LogWarning("Tried to change to the previous camera in managed Cam when there are no cameras setup.");
+                return;
+            }
+
+            int newInd = ActiveCameraIndex + -1;
+
+            if (newInd < 0)
+            {
+                ChangeActiveCamera(cameras.Count - 1);
+            }
+            else
+            {
+                ChangeActiveCamera(newInd);
+            }
+        }
+
+        /// <summary>
+        /// Add a camera to our managed camera collection.
+        /// </summary>
+        /// <param name="cam"></param>
+        public void AddCamera(ManagedCamera cam)
+        {
+            if (cam == null)
+            {
+                throw new System.ArgumentNullException(nameof(cam));
+            }
+
+            cameras.Add(cam);
+            cam.cameraIndex = cameras.Count - 1;
+        }
+
+        /// <summary>
+        /// Removes a camera from the camera collection and disables it. Does not destroy the camera.
+        /// </summary>
+        /// <param name="cameraIndex">Index of camera</param>
+        public void RemoveCamera(int cameraIndex)
+        {
+            if (cameraIndex < 0 || cameraIndex > cameras.Count - 1)
+            {
+                throw new System.ArgumentOutOfRangeException(nameof(cameraIndex));
+            }
+
+            cameras[cameraIndex].SetCameraActive(false);
+            cameras[cameraIndex].cameraIndex = -1;
+            cameras.RemoveAt(cameraIndex);
+        }
+
+        /// <summary>
+        /// Removes a camera from the camera collection and disables it. Does not destroy the camera.
+        /// </summary>
+        /// <param name="cam">Camera to remove</param>
+        public void RemoveCamera(ManagedCamera cam)
+        {
+            if (cam == null)
+            {
+                throw new System.ArgumentNullException(nameof(cam));
+            }
+
+            if (cam.cameraIndex < 0)
+            {
+                Debug.LogError("Camera passed in RemoveCamera() has not been assigned an valid index." +
+                    "Has this item not been added to the CameraManager yet?");
+                return;
+            }
+
+            RemoveCamera(cam.cameraIndex);
+        }
+    }
+}
--- LAUNCH Test Project/Assets/Scripts/ManagedCamera.cs
+++ LAUNCH Test Project/Assets/Scripts/ManagedCamera.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 
 namespace CameraToolkit
 {
+	/// <summary>
+	/// A sort of interface class that makes managing multiple cameras easier. 
+	/// Will automatically handle removing itself from the camera collection
+	/// </summary>
 	[RequireComponent(typeof(Camera))]
-	public class MultiCamera : MonoBehaviour
+	public class ManagedCamera : MonoBehaviour
 	{
         #region Serialized Fields
         [SerializeField]
@@ -14,18 +19,41 @@ namespace CameraToolkit
 
 		[Tooltip("The index for which this MultiCamera is stored in the MultiCameraManager. This will be set during runtime.")]
 		public int cameraIndex = -1;
-        #endregion
 
-        #region Properties
-        /// <summary>
-        /// The camera attached to this MultiCamera
-        /// </summary>
-        public Camera Camera => camera;
+		[SerializeField]
+		[Tooltip("Should this managed camera auto register itself to the camera manager on start?")]
+		private bool autoRegisterOnStart = false;
+
+		[SerializeField]
+		[Tooltip("Called when the camera has been enabled and is now the active camera.")]
+		[Space(20)]
+		private UnityEvent onCameraEnable;
+
+		[SerializeField]
+		[Tooltip("Called when the camera has been disabled and is no longer the active camera.")]
+		private UnityEvent onCameraDisable;
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// The camera attached to this MultiCamera
+		/// </summary>
+		public Camera Camera => camera;
 
 		/// <summary>
 		/// Is the current camera the active camera
 		/// </summary>
 		public bool IsCameraActive => camera.enabled;
+
+		/// <summary>
+		/// Called when the camera has been enabled and is now the active camera
+		/// </summary>
+		public UnityEvent OnCameraEnable => onCameraEnable;
+
+		/// <summary>
+		/// Called when the camera has been disabled and is no longer the active camera
+		/// </summary>
+		public UnityEvent OnCameraDisable => onCameraDisable;
 
 		/// <summary>
 		/// The target position the camera is trying to achieve.
@@ -66,6 +94,14 @@ namespace CameraToolkit
 		}
         #endregion
 
+        private void OnDestroy()
+        {
+            if (cameraIndex > -1)
+            {
+				CameraManager.Instance.RemoveCamera(cameraIndex);
+            }
+        }
+
         /// <summary>
         /// Moves the camera to the given position in world space. 
         /// It will interpolate to that position if there is an interpolator component is available to it.
@@ -100,17 +136,17 @@ namespace CameraToolkit
             }
         }
 
-		public void SetCameraActive(bool newState)
+		internal void SetCameraActive(bool newState)
         {
 			camera.enabled = newState;
 
 			if (newState)
             {
-				
+				onCameraEnable.Invoke();
             }
             else
             {
-
+				onCameraDisable.Invoke();
             }
         }
 
