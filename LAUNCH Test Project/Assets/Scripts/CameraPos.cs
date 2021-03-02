87c8131ce024480afa92f7f5adf3343c7fe94093
Andrew-Prudlow <22prudla1@elmbrookstudents.org>
2021-03-02 10:44:32 -0600

    Camera focus distance

---
+++ LAUNCH Test Project/Assets/Scripts/CameraPos.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CameraToolkit
+{
+	public class CameraPos : MonoBehaviour
+	{
+        public Interpolator optionalInterpolator;
+        public Button forwardButton;
+        public Button backwardsButton;
+        public int forwardDistance;
+        public int backwardsDistance;
+        private void AddPosition(Vector3 pos)
+        {
+            if (optionalInterpolator != null)
+            {
+                optionalInterpolator.targetPosition += pos;
+            }
+            else
+            {
+                transform.position += pos;
+            }
+            }
+        ///Vector3 endPos;
+        ///Vector3 startPos;
+        ///float lerpMult;
+        private void Start()
+        {
+            Button btn1 = forwardButton;
+            btn1.onClick.AddListener(CamForward);
+
+            Button btn2 = backwardsButton;
+            btn2.onClick.AddListener(CamBackwards);
+        }
+
+        void CamForward()
+        {
+            AddPosition(Camera.main.transform.forward * forwardDistance);
+        }
+
+        void CamBackwards()
+        {
+            AddPosition(Camera.main.transform.forward * -backwardsDistance);
+        }
+     
+    }
+}
